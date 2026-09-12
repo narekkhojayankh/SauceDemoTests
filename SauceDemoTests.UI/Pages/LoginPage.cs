3c0026cd4fa4d08741f193749f85d88a8b475111
Narek Khojayan <narekkhojayan.kh@gmail.com>
2026-09-12 22:45:56 +0400

    feat: add LoginPage and login tests (valid, invalid password, locked out)

---
+++ SauceDemoTests.UI/Pages/LoginPage.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using SauceDemoTests.UI.Core;
+
+namespace SauceDemoTests.UI.Pages
+{
+    public class LoginPage
+    {
+        private readonly IWebDriver _driver;
+
+        private By UsernameInput => By.Id("user-name");
+        private By PasswordInput => By.Id("password");
+        private By LoginButton => By.Id("login-button");
+        private By ErrorMessage => By.CssSelector("[data-test='error']");
+
+        public LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void EnterUsername(string username)
+        {
+            Waits.UntilVisible(_driver, UsernameInput).SendKeys(username);
+        }
+
+        public void EnterPassword(string password)
+        {
+            Waits.UntilVisible(_driver, PasswordInput).SendKeys(password);
+        }
+
+        public void ClickLogin()
+        {
+            Waits.UntilClickable(_driver, LoginButton).Click();
+        }
+
+        public void Login(string username, string password)
+        {
+            EnterUsername(username);
+            EnterPassword(password);
+            ClickLogin();
+        }
+
+        public string GetErrorText()
+        {
+            return Waits.UntilVisible(_driver, ErrorMessage).Text;
+        }
+    }
+}
---
+++ SauceDemoTests.UI/Tests/LoginTests .cs
@@ -0,0 +1,45 @@
+﻿using Allure.NUnit.Attributes;
+using NUnit.Framework;
+using SauceDemoTests.UI.Core;
+using SauceDemoTests.UI.Pages;
+
+namespace SauceDemoTests.UI.Tests
+{
+    [TestFixture]
+    [AllureSuite("Login")]
+    public class LoginTests : BaseTest
+    {
+        [Test]
+        [AllureFeature("Successful login")]
+        public void Login_WithValidCredentials_RedirectsToInventoryPage()
+        {
+            var loginPage = new LoginPage(Driver);
+
+            loginPage.Login("standard_user", "secret_sauce");
+
+            Assert.That(Driver.Url, Does.Contain("inventory.html"));
+        }
+
+        [Test]
+        [AllureFeature("Invalid password")]
+        public void Login_WithWrongPassword_ShowsErrorMessage()
+        {
+            var loginPage = new LoginPage(Driver);
+
+            loginPage.Login("standard_user", "wrong_password");
+
+            Assert.That(loginPage.GetErrorText(), Does.Contain("Username and password do not match"));
+        }
+
+        [Test]
+        [AllureFeature("Locked out user")]
+        public void Login_WithLockedOutUser_ShowsLockedOutMessage()
+        {
+            var loginPage = new LoginPage(Driver);
+
+            loginPage.Login("locked_out_user", "secret_sauce");
+
+            Assert.That(loginPage.GetErrorText(), Does.Contain("locked out"));
+        }
+    }
+}
